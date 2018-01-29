8c11f371144bbaea3b874bb6db3b41c5fbdc3243
Joshua Neighbarger <joshuaneigh@gmail.com>
2018-01-29 13:20:59 -0800

    Style chat and display to DOM as well as console

--- js/chat-client.js
+++ js/chat-client.js
@@ -1,26 +1,39 @@
-// #!javascript
-//
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content Type': 'text/plain'});
-//     res.write('Hello World!');
-//     res.end();
-// }).listen(8080);
+function addMessage(text, className) {
+    let node = document.getElementById("messages"),
+        message = document.createElement("div"),
+        wrapper = document.createElement("div");
+    wrapper.className = className;
+    message.appendChild(document.createTextNode(text));
+    wrapper.appendChild(message);
+    node.appendChild(wrapper);
+}
 
-let connection = new WebSocket("ws://localhost:9876");
+function sendMessage() {
+    let node = document.getElementById("message-input");
+    addMessage(node.value, "message-right");
+    connection.send(node.value);
+    node.value = "";
+}
 
-connection.onopen = function () {
-    connection.send("I HAVE CONNECTED");
-};
+function init() {
+    connection = new WebSocket("ws://localhost:9876");
+    connection.onopen = function () {
+        connection.send("I HAVE CONNECTED");
+    };
 
-connection.onerror = function (error) {
-    console.log(error);
-};
+    connection.onerror = function (error) {
+        console.log(error);
+    };
 
-connection.onmessage = function (e) {
-    console.log("Server: " + e.data);
-};
+    connection.onmessage = function (e) {
+        console.log("Server: " + e.data);
+        addMessage(e.data, "message-left");
+    };
 
-document.getElementById("send").addEventListener("click", function () {
-    connection.send(document.getElementById("message").value);
-});
+    document.getElementById("send").addEventListener("click", sendMessage);
+    document.getElementById("message-input").addEventListener("keydown", function (e) {
+        if (e.key === "Enter") sendMessage();
+    });
+}
+
+window.addEventListener("load", init);

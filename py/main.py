8fdb2bee3b7202dc51c28f54c2fee0cdd0989e3b
Joshua Neighbarger <joshuaneigh@gmail.com>
2018-01-30 00:45:50 -0800

    Clean up main

--- py/main.py
+++ py/main.py
@@ -1,4 +1,29 @@
+# Author: Joshua Neighbarger
+# Version: 30 January 2018
+# Email: jneigh@uw.edu
+
 import importlib
+
+# Color     Text    BG  |   Style
+# Black     30      40  |   No Effect   0
+# Red       31      41  |   Bold        1
+# Green     32      42  |   Underline   2
+# Yellow    33      43  |   Negative1   3
+# Blue      34      44  |   Negative2   4
+# Purple    35      45  |
+# Cyan      36      46  |
+# White     37      47  |
+TEXT_FORMAT = {
+    "input": "\033[1;32;40m"  # normal green_text black_background
+}
+MODULE_PKG = "modules"
+METADATA = {
+    "Authors": ("Joshua Neighbarger", "Karan Singla", "Zachary Chandler"),
+    "App Name": ("NLP Chat Console (Alan)",),
+    "Version": ("29 January 2018",)
+}
+HEADER = METADATA["App Name"][0] + ",  " + METADATA["Version"][0]
+PROMPT = ">>>:  "
 
 
 def launch(*args):
@@ -49,33 +74,12 @@ def main():
             COMMANDS[i[0]][0](i[1:])
 
 
-MODULE_PKG = "modules"
-METADATA = {
-    "Authors": ("Joshua Neighbarger", "Karan Singla", "Zachary Chandler"),
-    "App Name": ("NLP Chat Console (Alan)",),
-    "Version": ("29 January 2018",)
-}
-COMMANDS = {
-    "quit": (quit, 0, "Exit the program"),
-    "help": (show_help, 0, "Outputs each command and their description"),
-    "about": (info, 0, "Outputs the metadata of the application"),
-    "info": (info, 0, "Outputs the metadata of the application"),
-    "mod": (launch, -1, "Launches the specified module with the defined args")
-}
-# Color     Text    BG  |   Style
-# Black     30      40  |   No Effect   0
-# Red       31      41  |   Bold        1
-# Green     32      42  |   Underline   2
-# Yellow    33      43  |   Negative1   3
-# Blue      34      44  |   Negative2   4
-# Purple    35      45  |
-# Cyan      36      46  |
-# White     37      47  |
-TEXT_FORMAT = {
-    "input": "\033[1;32;40m"  # normal green_text black_background
-}
-HEADER = METADATA["App Name"][0] + ",  " + METADATA["Version"][0]
-PROMPT = ">>>:  "
-
 if __name__ == "__main__":
+    COMMANDS = {
+        "quit": (quit, 0, "Exit the program"),
+        "help": (show_help, 0, "Outputs each command and their description"),
+        "about": (info, 0, "Outputs the metadata of the application"),
+        "info": (info, 0, "Outputs the metadata of the application"),
+        "mod": (launch, -1, "Launches the specified module with the defined args")
+    }
     main()

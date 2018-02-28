6c1f517042a438f76897c82a148239612345d2ac
karan-singla <ksingla1@uw.edu>
2018-02-27 22:31:08 -0800

    added nltk- rake package
    https://github.com/csurfer/rake-nltk
    
    Use this to install the package.

--- py/objects/_parser.py
+++ py/objects/_parser.py
@@ -3,12 +3,21 @@
 # Version: 21 February 2018
 # Email: 
 
+from rake_nltk import Rake
+
 class Parser:
     def __init__(self):
         pass
 
     def parse(self, m):
-        return m
+        r = Rake()
+        r.extract_keywords_from_text(m)
+        return r.get_ranked_phrases()
     
 if __name__ == "__main__":
-    print("run test code here")
+    print("Testing....")
+    p = Parser()
+    testString = "Hello, How's everything"
+    output = p.parse(testString)
+    print(output)
+

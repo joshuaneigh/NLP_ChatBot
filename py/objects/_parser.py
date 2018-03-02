3a60f37522914ba7f69db131dfb0cf2c0cdae7f5
karan-singla <ksingla1@uw.edu>
2018-03-02 14:19:37 -0800

    Added more methods
    Added extractnoun

--- py/objects/_parser.py
+++ py/objects/_parser.py
@@ -4,6 +4,8 @@
 # Email: 
 
 from rake_nltk import Rake
+import nltk
+
 
 class Parser:
     def __init__(self):
@@ -12,12 +14,30 @@ class Parser:
     def parse(self, m):
         r = Rake()
         r.extract_keywords_from_text(m)
-        return r.get_ranked_phrases()
-    
+        words = r.get_ranked_phrases()
+        word = ""
+        for i in words:
+            word += i + " "
+        return word
+
+    def extractnoun(self, text):
+        Nouns = " "
+        for word, pos in nltk.pos_tag(nltk.word_tokenize(str(text))):
+            if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+                Nouns += word + " "
+        return Nouns
+
+
+
+
+
+
 if __name__ == "__main__":
     print("Testing....")
     p = Parser()
-    testString = "Hello, How's everything"
+    testString = "Hello, How are you?"
     output = p.parse(testString)
     print(output)
+    print('Extracted Nouns is ' + p.extractnoun(testString))
+
 
